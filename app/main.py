9eceea939d40c0fa26cc40757197e70b91d1d9c2
Lirik233 <kramarenko.vlad95@gmail.com>
2026-04-09 20:47:34 +0300

    done #2

--- app/main.py
+++ app/main.py
@@ -7,17 +7,15 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    person_list = []
+def create_person_list(people: list) -> list[Person]:
+    Person.people.clear()
 
-    for person_dict in people:
-        person = Person(person_dict["name"], person_dict["age"])
-        person_list.append(person)
+    person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person_dict, person in zip(people, person_list):
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            person.wife = Person.people[person_dict["wife"]]
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            person.husband = Person.people[person_dict["husband"]]
+        if person_dict.get("wife") is not None:
+            person.wife = Person.people[person_dict.get("wife")]
+        if person_dict.get("husband") is not None:
+            person.husband = Person.people[person_dict.get("husband")]
 
     return person_list
